c66d51cfd61d1d31510c0e239a94c0b82f0edbc7
starforce12345 <douglasbruno300@gmail.com>
2022-05-25 23:09:50 -0300

    imagens menu lateral

--- index.js
+++ index.js
@@ -20,49 +20,25 @@ const options = {
 	AllDates.push(data)
 
 	const Game1 = jogo[0]
-	const Game2 = jogo[1]
-	const Game3 = jogo[2]
 
 	console.log(Game1)
-	console.log(Game2)
-	console.log(Game3)
 
 	const bla1 = document.querySelector('.game1')
-
-	const bla2 = document.querySelector('.game2')
-
-	const bla3 = document.querySelector('.game3')
-
+	
 	const blake = document.createElement('div')
 
+	blake.style.backgroundImage = `url(${Game1.thumbnail})`
+
 	blake.setAttribute('class', 'bli')
-	blake.setAttribute('class', 'ble')
-	blake.setAttribute('class', 'blo')
 
 	bla1.appendChild(blake)
-
-	bla2.appendChild(blake)
-
-	bla3.appendChild(blake)
 
 	const blaki1 = document.createElement('h1')
 
 	blaki1.setAttribute('class', 'bli')
 
-	const blaki2 = document.createElement('h1')
-
-	blaki2.setAttribute('class', 'ble')
-
-	const blaki3 = document.createElement('h1')
-
-	blaki3.setAttribute('class', 'blo')
-
 	blaki1.innerHTML = Game1.title
-	blaki2.innerHTML = Game2.title
-	blaki3.innerHTML = Game3.title
 
 	blake.appendChild(blaki1)
-	blake.appendChild(blaki2)
-	blake.appendChild(blaki3)
 
 })
